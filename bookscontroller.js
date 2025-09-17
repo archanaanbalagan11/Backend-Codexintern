7e057c4ba412e82fe2283f8ed44824f759733252
ArchanaAnbalagan <archanaanbalagan04@gmail.com>
2025-09-17 12:15:40 +0530

    bookscontroller.js

---
+++ bookscontroller.js
@@ -0,0 +1,68 @@
+const Book = require("../models/Book");
+
+// Create book
+exports.createBook = async (req, res) => {
+    try {
+        const book = new Book(req.body);
+        await book.save();
+        res.status(201).json(book);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Get all books
+exports.getBooks = async (req, res) => {
+    try {
+        const books = await Book.find();
+        res.json(books);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// Get book by ID
+exports.getBookById = async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        res.json(book);
+    } catch (err) {
+        res.status(404).json({ error: "Book not found" });
+    }
+};
+
+// Update book
+exports.updateBook = async (req, res) => {
+    try {
+        const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(book);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Delete book
+exports.deleteBook = async (req, res) => {
+    try {
+        await Book.findByIdAndDelete(req.params.id);
+        res.json({ message: "Book deleted" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// Search book (by title or author)
+exports.searchBooks = async (req, res) => {
+    try {
+        const { query } = req.query;
+        const books = await Book.find({
+            $or: [
+                { title: { $regex: query, $options: "i" } },
+                { author: { $regex: query, $options: "i" } }
+            ]
+        });
+        res.json(books);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
