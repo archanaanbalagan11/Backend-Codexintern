0132785722d49ecf4682974e08734e436576801d
ArchanaAnbalagan <archanaanbalagan04@gmail.com>
2025-09-17 12:11:16 +0530

    Todo.js

---
+++ Todo.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: String,
+    isCompleted: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
